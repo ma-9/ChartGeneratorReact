aa492b8ad9745f6775265ea8d1c5e7c637d4244d
Manav Oza <manavoza99@gmail.com>
2020-01-21 21:44:37 +0530

    Updates

---
+++ precache-manifest.e32a447f2ef9b41152b90a972a2dd859.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5a88c07d43e76cf2f5a988fbf3ff19",
+    "url": "/ChartGeneratorReact/index.html"
+  },
+  {
+    "revision": "cc9854e4391cdb37723f",
+    "url": "/ChartGeneratorReact/static/css/main.b504eec6.chunk.css"
+  },
+  {
+    "revision": "b7300781eca8c7f170a5",
+    "url": "/ChartGeneratorReact/static/js/2.6f7178cc.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ChartGeneratorReact/static/js/2.6f7178cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc9854e4391cdb37723f",
+    "url": "/ChartGeneratorReact/static/js/main.c4e93af7.chunk.js"
+  },
+  {
+    "revision": "7768fb85f367fdbe7e01",
+    "url": "/ChartGeneratorReact/static/js/runtime-main.1b6112a6.js"
+  }
+]);
